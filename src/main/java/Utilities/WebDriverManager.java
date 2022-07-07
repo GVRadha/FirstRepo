5a795fa8ffedc3520cb65ff8b2181237adf02915
GVRadha <108509602+GVRadha@users.noreply.github.com>
2022-07-07 17:22:46 +0530

    Add files via upload

---
+++ src/main/java/Utilities/WebDriverManager.java
@@ -0,0 +1,22 @@
+package Utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebDriverManager {
+	
+	public WebDriver driver;
+	
+	public  WebDriverManager(WebDriver driver) {
+		
+		this.driver=driver;
+		
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+
+}
